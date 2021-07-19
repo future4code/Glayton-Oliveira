0c2d2eac0705b482add9ac0b21eb5a4659d74ead
GlaytonVerissimo <glayton.verissimo@hotmail.com>
2021-07-19 14:48:21 -0300

    Criei os arquivos

---
+++ semana6/renderizacao-condicional/renderizacao-condicional/src/components/PerguntaAberta.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function PerguntaAberta(props) {
+  return (
+    <div>
+      <p>{props.pergunta}</p>
+      <input onChange={props.onChange} value={props.value} />
+    </div>
+  );
+}
+
+
